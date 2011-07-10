34b4ef182ba822da850c757e626a8dd98b5f9582
Josh McArthur <joshua.mcarthur@gmail.com>
2011-07-10 23:12:59 +1200

    Refactor routing into polls app - completely decouple from project

--- urls.py
+++ urls.py
@@ -5,16 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    # Example:
-    # (r'^poll_tutorial/', include('poll_tutorial.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls)),
-)
+)   
+
